35bfaec74cb81b5d352a8ca0f41a2a9e4616deeb
greggs-patriot <rests_fours_0r@icloud.com>
2026-02-05 19:36:31 +0000

    contingent term repo

--- scripts/contingent_term_repo/process.py
+++ scripts/contingent_term_repo/process.py
@@ -5,7 +5,9 @@ path = os.path.join('raw',
                     'contingent_term_repo',
                     'contingent-term-repo-operations-results.xlsx')
 
-rates_path = os.path.join('raw','general','daily_bank_rate.csv')
+rates_path = os.path.join('processed','general','full_daily_bank_rate.csv')
+
+out_path = os.path.join('processed','contingent_term_repo','monthly_totals.csv')
 
 df = pd.read_excel(path,
                     header=1,
@@ -29,22 +31,33 @@ for _, row in df.iterrows():
         )
     )
 
-daily_df = pd.concat(daily_rows, ignore_index=True)
+daily_op_df = pd.concat(daily_rows, ignore_index=True)
 
 daily_rates = pd.read_csv(rates_path,
                     usecols=[0,1],
                     names=['date','bank_rate'],
                     header=0)
 
-daily_rates['date'] = pd.to_datetime(daily_rates['date'],format='%d %b %y')
-daily_rates['bank_rate'] = daily_rates['bank_rate'] / 100
-
-daily_df = daily_df.merge(daily_rates, on='date', how='left')
+daily_rates['date'] = pd.to_datetime(daily_rates['date'])
 
 
-daily_df['interest_total'] = (
-    (daily_df['allocated'] * (daily_df['bank_rate'] + daily_df['spread'])) / 365
+daily_op_df = daily_op_df.merge(daily_rates, on='date', how='left')
+
+
+daily_op_df['interest_total'] = (
+    (daily_op_df['allocated'] * (daily_op_df['bank_rate'] + daily_op_df['spread'])) / 365
 )
 
+daily_total : pd.DataFrame = daily_op_df.groupby('date', as_index=False)['interest_total'].sum()
 
-daily_df.to_csv('testing.csv')
+monthly  = (
+        daily_total
+        .set_index('date')
+        .resample('ME')['interest_total']
+        .sum()       
+        .round(3) 
+            
+)
+
+monthly = monthly.loc[monthly > 0]
+monthly.to_csv(out_path)
